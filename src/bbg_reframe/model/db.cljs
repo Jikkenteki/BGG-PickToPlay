21a56476169624620d2a7ac1b39cc405cc25e311
D Dranidis <dranidis@gmail.com>
2022-03-03 01:24:09 +0200

    Rename

--- src/bbg_reframe/model/db.cljs
+++ src/bbg_reframe/model/db.cljs
@@ -1,14 +1,11 @@
 (ns bbg-reframe.model.db
-  (:require   
-              [bbg-reframe.model.xmlapi :refer [game-attribute game-id
-                                                game-name game-rating game-my-rating
-                                                create-votes-for-results list-results-of-votes-per-playernum]]
-              [tubax.core :refer [xml->clj]]))
+  (:require [bbg-reframe.model.xmlapi :refer [game-attribute game-id game-name game-rating game-my-rating create-votes-for-results list-results-of-votes-per-playernum]]
+            [tubax.core :refer [xml->clj]]))
 
 ;; 
 ;; API - DB
 ;; 
-(defn  collection-item->game
+(defn  create-game-from-collection-item
   [collection-item]
   {:id (game-id collection-item)
    :name (game-name collection-item)
@@ -19,16 +16,16 @@
    :playingtime ((game-attribute collection-item) :playingtime)})
 
 (defn game-votes
-  [game]
+  [game-item]
   (into [] (map create-votes-for-results
-                (list-results-of-votes-per-playernum game))))
+                (list-results-of-votes-per-playernum game-item))))
 
 (defn indexed-games
   [response-xml]
   (let [response-clj (xml->clj response-xml)]
     ;; tag is expected to be items; otherwise an error occured
     (if (= :items (:tag response-clj))
-      (let [games (map collection-item->game (:content response-clj))
+      (let [games (map create-game-from-collection-item (:content response-clj))
             indexed-games (reduce
                            #(assoc %1 (:id %2) %2)
                            {} games)]
