79d3575d3b67f9c838d5f63680f3b066afaa4c68
D Dranidis <dranidis@gmail.com>
2022-03-06 23:14:45 +0200

    fix args for

--- src/bbg_reframe/components/sort_buttons_comp.cljs
+++ src/bbg_reframe/components/sort_buttons_comp.cljs
@@ -9,8 +9,12 @@
   (let [options (keys sorting-fun)
         value @(re-frame/subscribe [::subs/form :sort-id])]
     [:div.grid.grid-cols-2.grid-rows-2.gap-3.mb-1
-     (doall (for [option options]
-              ^{:key option}
-              [:div.button.flex {:class (when (= value option) "active")
-                                 :on-click #(re-frame/dispatch [::events/update-form :sort-id option])}
-               [:p.m-auto (name option)]]))]))
+     (doall (map
+             (fn [option]
+               ^{:key option}
+               [:div.button.flex {:class (when (= value option) "active")
+                                  :on-click #(re-frame/dispatch [::events/update-form :sort-id option])}
+                [:p.m-auto (name option)]]) options))]))
+
+
+
