5dd15d0a4adb7fceb20e1b9b6d9c0f021c311a9e
D Dranidis <dranidis@gmail.com>
2022-03-26 11:35:33 +0200

    Add links in searched games

--- src/bbg_reframe/components/search_results_comp.cljs
+++ src/bbg_reframe/components/search_results_comp.cljs
@@ -1,7 +1,8 @@
 (ns bbg-reframe.components.search-results-comp
   (:require
    [re-frame.core :as re-frame]
-   [bbg-reframe.subs :as subs]))
+   [bbg-reframe.subs :as subs]
+   [bbg-reframe.events :as events]))
 
 (defn search-results-comp []
   (let [search-results @(re-frame/subscribe [::subs/search-results])]
@@ -13,5 +14,6 @@
          [:li
           [:span
           ;;  (:id game) " : "
-           (:name game)]])
+           [:a {:on-click #(re-frame/dispatch [::events/navigate [:game-view :id (:id game)]])}
+            (:name game)]]])
        search-results)]]))
