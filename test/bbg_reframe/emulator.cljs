b33a8e2c810448965633bb004a876bbde30b980e
D Dranidis <dranidis@gmail.com>
2023-04-14 18:31:05 +0300

    Update fb api key in tests

--- test/bbg_reframe/emulator.cljs
+++ test/bbg_reframe/emulator.cljs
@@ -1,11 +1,14 @@
 (ns bbg-reframe.emulator
-  (:require [re-frame-firebase-nine.fb-reframe :refer [fb-reframe-config connect-emulator]]
-            [re-frame-firebase-nine.firebase-database :refer [fb-set! fb-ref get-db]]))
+  (:require [bbg-reframe.env-variables :refer [api-key-env]]
+            [re-frame-firebase-nine.fb-reframe :refer [connect-emulator
+                                                       fb-reframe-config]]
+            [re-frame-firebase-nine.firebase-database :refer [fb-ref fb-set!
+                                                              get-db]]))
 
 (defn connect-fb-emulator-empty-db
   []
   (fb-reframe-config {:temp-path [:firebase-temp-storage]
-                      :firebase-config {:apiKey "AIzaSyCLH4BlNSOfTrMlB_90Hsxg5cr3bn3p-7E",
+                      :firebase-config {:apiKey api-key-env,
                                         :authDomain "help-me-pick-what-to-play.firebaseapp.com",
                                         :databaseURL "https://help-me-pick-what-to-play-default-rtdb.europe-west1.firebasedatabase.app",
                                         :projectId "help-me-pick-what-to-play",
